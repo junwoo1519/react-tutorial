cf1e802d0c293d0c0c5fc83f680b67a5308567bd
junwoo <junwoo101797@gmail.com>
2022-11-22 12:32:21 +0900

    무비 리스트 css추가

--- movie-project/src/App.js
+++ movie-project/src/App.js
@@ -1,14 +1,20 @@
+import React from "react";
 import './App.css';
+
+import data from "./data.json";
+
 import Header from "./header/header";
-import styled from "styled-components";
+import MovieList from  "./movieList/movieList"
 
 function App() {
   const state = {
+    movieData: data,
     headerMessage: "영화 소개 프로젝트"
   };
   return (
     <div className="App">
       <Header headerMessage={state.headerMessage}/>
+      <MovieList movieData={state.movieData} />
     </div>
   );
 }
